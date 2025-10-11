f2dfc6ade1d43d6e5177bc952c0eea7e00763467
AdityaTaggar05 <aditya05.mgg@gmail.com>
2025-10-12 04:42:04 +0530

    [server] added route to fetch user info

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"github.com/AdityaTaggar05/Verto/server/database"
+	"github.com/AdityaTaggar05/Verto/server/services"
 	"github.com/AdityaTaggar05/Verto/server/utils"
 	"github.com/gofiber/fiber/v3"
 )
@@ -11,6 +13,16 @@ func RegisterRoutes(app *fiber.App) {
 	})
 
 	api := app.Group("/api")
+
+	api.Get("/", services.AuthMiddleware, func(c fiber.Ctx) error {
+		user, err := database.FetchUserFromUID(c, c.Locals("uid").(string))
+
+		if err != nil {
+			return utils.RespondError(c, fiber.StatusInternalServerError, "couldn't fetch the user")
+		}
+		return utils.RespondSuccess(c, fiber.StatusOK, "fetched user successfully", user)
+	})
+
 	RegisterAuthRoutes(api.Group("/auth"))
 	RegisterSessionRoutes(api.Group("/sessions"))
 	RegisterWardrobeRoutes(api.Group("/wardrobe"))
